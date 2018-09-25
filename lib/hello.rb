b2acb4eb5d5ab6eae7de0c933b9c85f6ab1013ff
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 00:01:44 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,10 +2,11 @@ def hello_t(array)
   # puts "Hey! No block was given!" if blk.nil?
   # return "Hey! No block was given!" if blk.nil?
   array = self if array.nil?
-  array.each do |w|
-    yield(w)
+  if block_given?
+    array.each { |w| yield(w) }
+  else 
+    puts "Hey! No block was given!"
   end 
-
 end 
 
 # arr = ["Tim", "Tom", "Jim"] 
